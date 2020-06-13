e22d7fb96eefff425ddec7eb0442c264ba6e2176
David Flores <drfloresj@gmail.com>
2020-06-13 12:59:15 -0700

    fixing folder structure

--- index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import "./index.css";
-
-// renders the default page?
-ReactDOM.render(<App />, document.getElementById("root"));
